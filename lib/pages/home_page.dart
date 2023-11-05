715b1c49f771f9751e63ff8402bd7d17e167d014
Karina <karina93209@gmail.com>
2023-11-06 00:43:31 +0100

    creating nav bar and sketches of pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:e_commerce_app/components/bottom_nav_bar.dart';
+import 'package:e_commerce_app/pages/shop_page.dart';
 import 'package:flutter/material.dart';
+
+import 'cart_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  final List<Widget> pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      bottomNavigationBar: MyBottomNavBar(onTabChange: (index) => navigateBottomBar(index),),
+      body: pages[selectedIndex],
+    );
   }
 }
